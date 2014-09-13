321536d723d36432588c3f580c68daa22d3e56ed
Dan McGrath <danmcg.au@gmail.com>
2014-09-12 22:27:20 -0600

    Drive code to its own JS file
    Refactored Drive code to its own JS file so it can be re-used. Created
    -next version of dynamic-frame to test it out without affecting 'prod'.
    Added integration code to analysis.html to test.

---
+++ pgn4googledrive.js
@@ -0,0 +1,67 @@
+
+
+// Drive API information needed to identify this app.
+var CLIENT_ID = '1023257958231-8nav97ck2tohrhvfguecrvo03qe3t2ie.apps.googleusercontent.com';
+var SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install', 'https://www.googleapis.com/auth/userinfo.profile'];
+
+// Accept the JSON state variable that is passed along by Google Drive when opening a PGN file
+function start_drive() {
+  var pgnDrive = null;
+  thisRegExp = /(&|\?)(state)=([^&]*)(&|$)/i;
+  if (thisParamString.match(thisRegExp) !== null) {
+    var stateJSON = unescape(thisParamString.match(thisRegExp)[3]);
+    pgnDrive = JSON.parse(stateJSON).ids[0];
+  }
+  getAuth(pgnDrive);
+}
+
+// Check if we have authorization to the user's Drive account to retrieve the PGN file
+// If not, call ask the user for authorization to do so.
+// Generally, we only need to ask the user the very first time they access the app.
+function getAuth(fileID) {
+  var driveData = {pgnDrive: fileID};
+  var handleRes = handleAuthResult.bind(driveData)
+  gapi.auth.authorize({'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true}, handleRes);
+}
+
+// Called by getAuth to handle the result of checking for authorization,
+// asking the user if we don't already have it.
+function handleAuthResult(authResult) {
+  if (!authResult || authResult.error) {
+    var handleRes = handleAuthResult.bind(this)
+    gapi.auth.authorize({'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false}, handleRes);
+  } else {
+    var loadPgn = getPgn.bind(this);
+    gapi.client.load('drive', 'v2', loadPgn);
+  }
+}
+
+// Using the file id passed in the state variable from Drive, attempt to get the PGN file.
+// Once completed, start the pgn4web engine.
+function getPgn() {
+      var request = gapi.client.drive.files.get({'fileId': this.pgnDrive});
+      request.execute(function(resp) {
+        downloadFile(resp, start_pgn4web);
+      });
+}
+
+// Retrieve the PGN file's bytes from Google Drive.
+// Store the PGN data in the pgnText TextArea so the pgn4web engine can load it
+function downloadFile(file, callback) {
+  if (file.downloadUrl) {
+    var accessToken = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file.downloadUrl);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.onload = function() {
+      document.getElementById("pgnText").value = xhr.responseText;
+      callback();
+    };
+    xhr.onerror = function() {
+      callback();
+    };
+    xhr.send();
+  } else {
+    callback();
+  }
+}
